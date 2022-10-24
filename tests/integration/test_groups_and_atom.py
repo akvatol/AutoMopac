ce044440c84aae9c50d7c5ad6aa659bccbd3c368
Anton D <a.v.daomnin@gmail.com>
2022-10-24 07:54:46 +0300

    More tests

--- tests/integration/test_groups_and_atom.py
+++ tests/integration/test_groups_and_atom.py
@@ -2,7 +2,14 @@ import pytest
 from src.Basic.Atom import Atom
 from src.Basic.symmetry_element import symmetry_element
 from src.Symmetry.PointGroups import PointGroup
+from src.Symmetry.ScrewAxis import ScrewAxis
 
+
+SAs = (ScrewAxis(q=1, p=1, A=10),
+       ScrewAxis(q=2, p=1, A=10),
+       ScrewAxis(q=3, p=1, A=12),
+       ScrewAxis(q=17, p=6, A=0.100754840323E+02),
+       ScrewAxis(q=28, p=17, A=0.167892743723E+02))
 
 @pytest.mark.parametrize('PG, atom, expected', [
     (PointGroup(), A:= Atom.from_string('C 0 0 0'), {A: [A]}),
@@ -14,7 +21,14 @@ from src.Symmetry.PointGroups import PointGroup
     (PointGroup(n=2, I=True), A:= Atom.from_string('C 2 0 1'), {A: [A, Atom.from_string('C -2 0 -1'), Atom.from_string('C 2 0 -1'), Atom.from_string('C -2 0 1')]}),
     (PointGroup(n=2, I=True, h=True), A:= Atom.from_string('C 2 0 1'), {A: [A, Atom.from_string('C -2 0 -1'), Atom.from_string('C 2 0 -1'), Atom.from_string('C -2 0 1')]}),
     ])
-def test_get_orbit(PG, atom, expected):
+def test_PG_get_orbit(PG, atom, expected):
     assert len(PG.get_orbit(atom).get(atom)) == len(PG.get_orbit(atom).get(atom))
     assert set(PG.get_orbit(atom).get(atom)) == set(expected.get(atom))
 
+@pytest.mark.parametrize("SA,atom,expected", [
+    (SAs[0], A:= Atom.from_string('C 2 0 1'), {A:[A]}),
+    (SAs[1], A:= Atom.from_string('C 2 0 1'), {A:[A, Atom.from_string('C 7 0 -1')]}),
+    (SAs[2], A:= Atom.from_string('C 2 0 0'), {A:[A, Atom.from_string('C 6 0 0'), Atom.from_string('C 10 0 0')]}),
+])
+def test_SA_get_orbit(SA, atom, expected):
+    assert set(SA.get_orbit(atom).get(atom)) == set(expected.get(atom))
